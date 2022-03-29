8deae71c443b84a3e4d5518e0b34862c53122f5b
unknown <aaakrad@gmail.com>
2022-03-30 00:04:12 +0300

    root static files in setigs

--- workshop/petstagram/petstagram/settings.py
+++ workshop/petstagram/petstagram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,7 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT = BASE_DIR /'staticfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'mediafiles',
+)
+
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles/'
 MEDIA_URL = '/media/'
