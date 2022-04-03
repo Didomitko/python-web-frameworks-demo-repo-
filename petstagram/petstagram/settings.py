d14df9d455ed261a2999ee196df099151fda2541
unknown <aaakrad@gmail.com>
2022-04-03 17:48:49 +0300

    Extract DEBUG

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)22kgh$9$&q2dkgi)xhf+*j9jf0n9&x3^%(e=q6hblrwo=28x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'localhost',
